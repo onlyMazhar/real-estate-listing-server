c383eda8738318880a09e3343c30c7b84d398cfa
Mazhar <mi.sabbir420@gmail.com>
2025-11-12 18:06:42 +0600

    post api created for property listing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const port = process.env.port || 3000; 
+const port = process.env.port || 3000;
 
 app.use(cors())
 app.use(express.json())
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 //BocHY9k6oJWI5YSk
-const uri = "mongodb+srv://real-estate-db:passhere@cluster0.81efjl7.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://real-estate-db:BocHY9k6oJWI5YSk@cluster0.81efjl7.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,9 +27,21 @@ async function run() {
     const db = client.db('real-estate-db')
     const listCollections = db.collection('property_listing')
 
-    app.get('/lists', async (req, res)=>{
+    app.get('/lists', async (req, res) => {
       const result = await listCollections.find().toArray()
       res.send(result)
+    })
+
+    app.post('/lists', async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = listCollections.insertOne(data);
+
+      res.send({
+        success: true,
+        result
+
+      })
     })
 
 
@@ -53,14 +65,14 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('Connection is OK')
+app.get('/', (req, res) => {
+  res.send('Connection is OK')
 })
 
-app.get('/lists', (req, res)=>{
-    res.send('List of proparties')
+app.get('/lists', (req, res) => {
+  res.send('List of proparties')
 })
 
-app.listen(port,()=>{
-    console.log(`Listning from ${port}`)
+app.listen(port, () => {
+  console.log(`Listning from ${port}`)
 })
